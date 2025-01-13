45f1c312fbcf3409da13ab180773aef745c88e80
Sebastián <calderonsebastian211@gmail.com>
2025-01-12 20:55:04 -0500

    Añade plantillas y modifica views para la gestión de facturas - Crea plantillas: crear_factura.html, editar_factura.html, gestion_factura.html- Modifica admin.py, views.py y otros archivos relacionados

--- facturacion/models.py
+++ facturacion/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Pago(models.Model):
+    cliente = models.CharField(max_length=100)
+    correo = models.EmailField(null=True, blank=True)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    estado = models.CharField(
+        max_length=10,
+        choices=[("Pendiente", "Pendiente"), ("Pagado", "Pagado")],
+        default="Pendiente"
+    )
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self):
+        return f"{self.cliente} - {self.monto}"
--- facturacion/views.py
+++ facturacion/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Pago
+def gestion_pagos(request):
+    pagos = Pago.objects.all()
+    return render(request, 'facturacion/gestion_factura.html', {'pagos': pagos})
 
-# Create your views here.
+def marcar_pagado(request, id):
+    if request.method == 'POST':
+        pago = get_object_or_404(Pago, id=id)
+        if pago.estado != 'Pagado':
+            pago.estado = 'Pagado'
+            pago.save()
+        return redirect('gestion_pagos')
+    return redirect('gestion_pagos')
+
+def editar_pago(request, pk):
+    pago = get_object_or_404(Pago, pk=pk)
+    if request.method == 'POST':
+        estado = request.POST.get('estado')
+        pago.estado = estado
+        pago.save()
+        return redirect('gestion_pagos')
+    return render(request, 'facturacion/editar_factura.html', {'pago': pago})
+
+def eliminar_pago(request, pk):
+    pago = get_object_or_404(Pago, pk=pk)
+    if request.method == 'POST':
+        pago.delete()
+        return redirect('gestion_pagos')
+
+
+def crear_factura(request):
+    if request.method == 'POST':
+        cliente = request.POST.get('cliente')
+        correo = request.POST.get('correo')
+        monto = request.POST.get('monto')
+        estado = request.POST.get('estado')
+        Pago.objects.create(cliente=cliente, correo=correo, monto=monto, estado=estado)
+        return redirect('gestion_pagos')
+    return render(request, 'facturacion/crear_factura.html')
