d91b89bc537d33991ac57e508596f4eb84d64920
Alex Ramirez <alexis.rj2110@gmail.com>
2025-01-12 22:47:51 -0500

    Completamos el CRUD de areas Optimizamos uso de html, evitando repeticiones. Completamos vistas del controlador para el CRUD. Añadimos mensajes de éxito o error al hacer las acciones del CRUD. Optimizamos el uso y presentación del formulario. Detalles extras en la UI. Solución de Errores.

--- areas/forms.py
+++ areas/forms.py
@@ -7,9 +7,54 @@ class AreaCreationForm(forms.ModelForm):
     class Meta:
         model = Area
         fields = ['name', 'description', 'address', 'city', 'opening_time',
-                 'closing_time', 'has_parking', 'has_showers', 'has_lockers',
-                 'has_equipment', 'images']
+                  'closing_time', 'has_parking', 'has_showers', 'has_lockers',
+                  'has_equipment', 'images']
         widgets = {
-            'opening_time': forms.TimeInput(attrs={'type': 'time'}),
-            'closing_time': forms.TimeInput(attrs={'type': 'time'}),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'opening_time': forms.TimeInput(attrs={
+                'class': 'form-control',
+                'type': 'time',
+            }),
+            'closing_time': forms.TimeInput(attrs={
+                'class': 'form-control',
+                'type': 'time',
+            }),
+            'address': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 1,
+
+            }),
+            'city': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 1,
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Escribe la descripción aquí...',
+            }),
+            'has_parking': forms.CheckboxInput(attrs={
+                'class': 'form-check-input',
+            }),
+            'has_lockers': forms.CheckboxInput(attrs={
+                'class': 'form-check-input',
+            }),
+            'has_showers': forms.CheckboxInput(attrs={
+                'class': 'form-check-input',
+            }),
+            'has_equipment': forms.CheckboxInput(attrs={
+                'class': 'form-check-input',
+            }),
+            'images': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'type': 'file',
+            })
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['images'].widget.initial_text = "Actual"
+        self.fields['images'].widget.clear_checkbox_label = "Eliminar"
+        self.fields['images'].widget.input_text = "Cambiar"
